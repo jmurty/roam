9421e50d506834f24c4c13129563f88297e37913
James Murty <james@murty.co>
2019-05-27 23:06:05 +1000

    Improve naming of GitHub-derived test data

--- test_roam.py
+++ test_roam.py
@@ -5,7 +5,7 @@ from roam import r, MISSING, Roamer
 
 # Amended subset of GitHub user data for 'jmurty' from
 # https://api.github.com/users/jmurty/repos
-data = [
+github_data = [
     {
         "name": "java-xmlbuilder",
         "full_name": "jmurty/java-xmlbuilder",
@@ -73,7 +73,7 @@ data = [
     },
 ]
 
-data0 = data[0]
+github_data0 = github_data[0]
 
 # Select data from https://en.wikipedia.org/wiki/Monty_Python_filmography
 python_filmography = [
@@ -102,133 +102,137 @@ class TestRoam:
         assert not MISSING
 
     def test_getattr_traversal(self):
-        assert r(data0).license == data0["license"]
+        assert r(github_data0).license == github_data0["license"]
 
-        assert r(data0).license.name == data0["license"]["name"]
+        assert r(github_data0).license.name == github_data0["license"]["name"]
 
-        assert r(data0).license.name  # Truthy
+        assert r(github_data0).license.name  # Truthy
 
     def test_attr_traversal_missing(self):
-        assert r(data0).x == MISSING
+        assert r(github_data0).x == MISSING
 
-        assert r(data0).license.x == MISSING
+        assert r(github_data0).license.x == MISSING
 
-        assert not r(data0).license.x  # Falsey
+        assert not r(github_data0).license.x  # Falsey
 
     def test_getitem_traversal(self):
-        assert r(data0)["license"] == data0["license"]
+        assert r(github_data0)["license"] == github_data0["license"]
 
-        assert r(data0)["license"]["name"] == data0["license"]["name"]
+        assert r(github_data0)["license"]["name"] == github_data0["license"]["name"]
 
-        assert r(data0).license.name[0] == data0["license"]["name"][0]
+        assert r(github_data0).license.name[0] == github_data0["license"]["name"][0]
 
-        assert r(data0).license.name[-1] == data0["license"]["name"][-1]
+        assert r(github_data0).license.name[-1] == github_data0["license"]["name"][-1]
 
-        assert r(data0).license.name[-1]  # Truthy
+        assert r(github_data0).license.name[-1]  # Truthy
 
     def test_getitem_traversal_missing(self):
-        assert r(data0)["x"] == MISSING
+        assert r(github_data0)["x"] == MISSING
 
-        assert r(data0)["license"]["x"] == MISSING
+        assert r(github_data0)["license"]["x"] == MISSING
 
-        assert r(data0)["license"]["name"]["x"] == MISSING
+        assert r(github_data0)["license"]["name"]["x"] == MISSING
 
-        assert not r(data0)["license"]["name"]["x"]  # Falsey
+        assert not r(github_data0)["license"]["name"]["x"]  # Falsey
 
     def test_getattr_and_getitem_traversal(self):
-        assert r(data0).license["name"] == data0["license"]["name"]
+        assert r(github_data0).license["name"] == github_data0["license"]["name"]
 
-        assert r(data0)["license"].name == data0["license"]["name"]
+        assert r(github_data0)["license"].name == github_data0["license"]["name"]
 
-        assert r(data0)["license"].name[1] == data0["license"]["name"][1]
+        assert r(github_data0)["license"].name[1] == github_data0["license"]["name"][1]
 
     def test_getattr_and_getitem_traversal_missing(self):
-        assert r(data0).license["x"] == MISSING
+        assert r(github_data0).license["x"] == MISSING
 
-        assert r(data0)["license"].x == MISSING
+        assert r(github_data0)["license"].x == MISSING
 
     def test_slice_traversal(self):
-        assert r(data)[:] == data[:]
+        assert r(github_data)[:] == github_data[:]
 
-        assert r(data)[1:] == data[1:]
+        assert r(github_data)[1:] == github_data[1:]
 
     def test_slice_traversal_missing(self):
-        assert len(data) == 2
+        assert len(github_data) == 2
 
-        assert r(data)[3] == MISSING
+        assert r(github_data)[3] == MISSING
 
-        assert r(data)[3:] == []
+        assert r(github_data)[3:] == []
 
-        assert r(data)[2:4] == data[2:4]
+        assert r(github_data)[2:4] == github_data[2:4]
 
     def test_call_returns_item(self):
-        assert r(data0).license.name() is data0["license"]["name"]
+        assert r(github_data0).license.name() is github_data0["license"]["name"]
 
-        assert r(data0)["license"]["name"]() is data0["license"]["name"]
+        assert r(github_data0)["license"]["name"]() is github_data0["license"]["name"]
 
     def test_call_returns_item_missing(self):
-        assert r(data0).x() == MISSING
-        assert r(data0)["x"]() is MISSING
+        assert r(github_data0).x() == MISSING
+        assert r(github_data0)["x"]() is MISSING
 
-        assert r(data0).license.x() == MISSING
-        assert r(data0)["name"].x() is MISSING
+        assert r(github_data0).license.x() == MISSING
+        assert r(github_data0)["name"].x() is MISSING
 
     def test_call_delegates_to_and_returns_item(self):
         # Delegate to methods on `dict` item
-        assert r(data0)["license"].items() == data0["license"].items()
-        assert r(data0).license.keys() == data0["license"].keys()
-        assert list(r(data0).license.values()) == list(data0["license"].values())
+        assert r(github_data0)["license"].items() == github_data0["license"].items()
+        assert r(github_data0).license.keys() == github_data0["license"].keys()
+        assert list(r(github_data0).license.values()) == list(
+            github_data0["license"].values()
+        )
 
         # Delegate to methods on `str` item
-        assert r(data0).license.url.split("/") == [
+        assert r(github_data0).license.url.split("/") == [
             "https:",
             "",
             "api.github.com",
             "licenses",
             "apache-2.0",
         ]
-        assert type(r(data0).license.url.split("/")) is list
+        assert type(r(github_data0).license.url.split("/")) is list
 
         # Delegate to callable item within traversal
-        assert r(data0).owner.fn("Hi") == "Hi"
-        assert r(data0).owner.fn(999) == 999
+        assert r(github_data0).owner.fn("Hi") == "Hi"
+        assert r(github_data0).owner.fn(999) == 999
 
     def test_call_with_roam_option(self):
-        assert isinstance(r(data0).license.items(_roam=True), Roamer)
-        assert r(data0).license.items(_roam=True) == data0["license"].items()
+        assert isinstance(r(github_data0).license.items(_roam=True), Roamer)
+        assert (
+            r(github_data0).license.items(_roam=True) == github_data0["license"].items()
+        )
 
     def test_call_on_missing_with_roam_option(self):
-        assert isinstance(r(data0).x.items(_roam=True), Roamer)
-        assert r(data0).x.items(_roam=True) == MISSING
+        assert isinstance(r(github_data0).x.items(_roam=True), Roamer)
+        assert r(github_data0).x.items(_roam=True) == MISSING
 
     def test_call_with_invoke_option(self):
-        assert r(data0).owner.login(_invoke=len) == 6
+        assert r(github_data0).owner.login(_invoke=len) == 6
 
     def test_call_with_invoke_and_roam_options(self):
-        assert isinstance(r(data0).owner(_invoke=len, _roam=True), Roamer)
-        assert r(data0).owner(_invoke=len, _roam=True) == 4
+        assert isinstance(r(github_data0).owner(_invoke=len, _roam=True), Roamer)
+        assert r(github_data0).owner(_invoke=len, _roam=True) == 4
 
     def test_iterator_traversal(self):
-        for i, item_roamer in enumerate(r(data)):
+        for i, item_roamer in enumerate(r(github_data)):
             assert isinstance(item_roamer, Roamer)
-            assert item_roamer.name == data[i]["name"]
+            assert item_roamer.name == github_data[i]["name"]
 
         # Trying to iterating over non-iterable
-        for item_roamer in r(data0).size:
+        for item_roamer in r(github_data0).size:
             pytest.fail("Shouldn't be able to iterate over int")
 
-        for item_roamer in r(data0).fork:
+        for item_roamer in r(github_data0).fork:
             pytest.fail("Shouldn't be able to iterate over bool")
 
     def test_iterator_traversal_missing(self):
-        for item_roamer in r(data0).x:
+        for item_roamer in r(github_data0).x:
             pytest.fail("Shouldn't be able to iterate over MISSING")
 
-        for item_roamer in r(data0).license.name.x:
+        for item_roamer in r(github_data0).license.name.x:
             pytest.fail("Shouldn't be able to iterate over MISSING")
 
     def test_nested_iterable_traversal(self):
-        assert r(data)[:]["owner"]["login"] == ["jmurty", "jmurty"]
+        assert r(github_data)[:]["owner"]["login"] == ["jmurty", "jmurty"]
 
         assert r(python_filmography)[:]["title"] == [
             "Monty Python's Flying Circus",
